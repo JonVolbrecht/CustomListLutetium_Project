c2cb10e708beddcb9c076ffd42e8dd11b830f1c6
JonVolbrecht <jonathanvolbrecht@yahoo.com>
2021-02-22 14:14:28 -0600

    Added Add method tests. Incorporated demo material into CustomList class. Will make each function its own class once I start moving into that area of project.

--- CustomListLutetium/CustomList.cs
+++ CustomListLutetium/CustomList.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace CustomListLutetium
 {
-    class CustomList
+    public class CustomList<T>
     {
+        //member variable
+        private T[] _items;
+
+        private int count;
+        private int capacity;
+
+        public CustomList()
+        {
+            count = 0;
+            capacity = 4;
+            _items = new T[capacity];
+        }
+
+        public void Add(T itemToAdd)
+        {
+            if (count == capacity)
+            {
+                //create new  array
+                //move items over
+                //new array is now _items array.
+            }
+            _items[count] = itemToAdd;
+            count++;
+        }
+
     }
 }
--- CustomListLutetiumTests/AddMethodTestMethods.cs
+++ CustomListLutetiumTests/AddMethodTestMethods.cs
@@ -1,14 +1,64 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using CustomListLutetium;
 
 namespace CustomListLutetiumTests
 {
     [TestClass]
-    public class UnitTest1
+    public class AddMethodTestMethods
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Add_VerifyCountAddition()
         {
+            //Arrange
+            CustomList<int> test = new CustomList<int>();
+            int expected = 1;
+            int actual;
+
+            //Act
+            actual = test.Add(2);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
         }
+
+        public void Add_VerifyIndexValueZero()
+        {
+            //Arrange
+            CustomList<int> test = new CustomList<int>();
+            int expected = 2;
+            int actual;
+
+            //Act
+            actual = test.Add(expected);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        public void Add_VerifyIndexValueOne()
+        {
+            //Arrange
+            CustomList<int> test = new CustomList<int>();
+            int expected1 = 2;
+            int expected2 = 16
+            
+            //Act
+
+
+            //Assert
+        }
+
+
+
+
+
+
+
+
+
+
     }
 }
