e44d462932b6b29f0b973ed22a4e84a125be51b3
JonVolbrecht <jonathanvolbrecht@yahoo.com>
2021-02-24 10:10:31 -0600

    Second Remove test method

--- CustomListLutetiumTests/RemoveMethodTestMethods.cs
+++ CustomListLutetiumTests/RemoveMethodTestMethods.cs
@@ -10,26 +10,50 @@ namespace CustomListLutetiumTests
     public class RemoveMethodTestMethods
     {
         [TestMethod]
-        public void Remove_CheckAtIndexZero()
+        public void Remove_VerifyAtIndexZero()
         {
             //Arrange
             CustomList<int> test = new CustomList<int>();
             test.Add(14);
             test.Add(10);
-            int expectedResult = 10;
+            int expected = 10;
+
 
             //Act
             test.Remove(14);
-            int actualResult = test[0];
+            int actual = test[0];
 
             //Assert
-            Assert.AreEqual(expectedResult, actualResult);
+            Assert.AreEqual(expected, actual);
 
         }
 
+        [TestMethod]
+        public void Remove_VerifyAtIndexSeven()
+        {
+            //Arrange
+            CustomList<int> test = new CustomList<int>();
+            test.Add(10);
+            test.Add(11);
+            test.Add(12);
+            test.Add(13);
+            test.Add(14);
+            test.Add(15);
+            test.Add(16);
+            test.Add(17);
+            test.Add(18);
+            int expected = 18;
 
+            //Act
+            test.Remove(17);
+            int actual = test[7];
 
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
 
+        [TestMethod]
+        public void Remove_
 
 
 
