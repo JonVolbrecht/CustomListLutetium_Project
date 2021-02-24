28de3bca9a803d9a0a61409828796b42f0e0fec4
JonVolbrecht <jonathanvolbrecht@yahoo.com>
2021-02-24 10:01:27 -0600

    First Remove test method added.

--- CustomListLutetiumTests/RemoveMethodTestMethods.cs
+++ CustomListLutetiumTests/RemoveMethodTestMethods.cs
@@ -9,7 +9,23 @@ namespace CustomListLutetiumTests
     [TestClass]
     public class RemoveMethodTestMethods
     {
+        [TestMethod]
+        public void Remove_CheckAtIndexZero()
+        {
+            //Arrange
+            CustomList<int> test = new CustomList<int>();
+            test.Add(14);
+            test.Add(10);
+            int expectedResult = 10;
 
+            //Act
+            test.Remove(14);
+            int actualResult = test[0];
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
 
 
 
