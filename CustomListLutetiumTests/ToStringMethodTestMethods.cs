8133bd0b360c012a02fb14bb6851a085deb6bfdd
JonVolbrecht <jonathanvolbrecht@yahoo.com>
2021-02-25 11:56:53 -0600

    First ToString test method completed.

--- CustomListLutetiumTests/ToStringMethodTestMethods.cs
+++ CustomListLutetiumTests/ToStringMethodTestMethods.cs
@@ -9,16 +9,28 @@ namespace CustomListLutetiumTests
     class ToStringMethodTestMethods
     {
         [TestMethod]
+        public void ToString_VerifyConversionToStrings()
+        {
+            //Arrange
+            CustomList<int> test = new CustomList<int>();
 
+            int itemsToAdd = 10;
+            string expected = "{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }";
 
+            for (int i = 0; i < itemsToAdd; i++)
+            {
+                test.Add(i);
+            }
 
+            //Act
+            string actual = test.ToString();
 
+            //Assert
+            Assert.AreEqual(expected, actual);
 
+        }
 
-
-
-
-
+        [TestMethod]
 
 
 
