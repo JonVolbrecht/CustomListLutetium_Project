cb3d51d48d57014272a9e76aa737e9ea66fbd669
JonVolbrecht <jonathanvolbrecht@yahoo.com>
2021-03-01 13:58:19 -0600

    SubtractMethod test method 1 completed.

--- CustomListLutetiumTests/SubtractOperatorTestMethods.cs
+++ CustomListLutetiumTests/SubtractOperatorTestMethods.cs
@@ -11,26 +11,29 @@ namespace CustomListLutetiumTests
         public void SubtractOperator_VerifyListsCombineAtIndexZero()
         {
             //Arrange
-            int itemsToRemove = 4;
             CustomList<int> test1 = new CustomList<int>();
             CustomList<int> test2 = new CustomList<int>();
             CustomList<int> test3;
 
-            test1.Add( 1, 3, 5, 7 );
-            
-            test2.Add( 2, 4, 6, 8 );
-            
-            int expectedResult = 0;
+            test1.Add(1);
+            test1.Add(3);
+            test1.Add(5);
+            test1.Add(7);
+            test2.Add(2);
+            test2.Add(4);
+            test2.Add(6);
+            test2.Add(8);
 
             //Act
             test3 = test1 - test2;
-            int actualResult = test3[0];
+            int expected = 0;
+            int actual = test3[0];
 
             //Assert
-            Assert.AreEqual(expectedResult, actualResult);
+            Assert.AreEqual(expected, actual);
         }
 
-
+        
 
 
 
